5e08bc7040a858e158883ed4b956ccc3cf2815a6
unknown <kraynevroman@gmail.com>
2020-02-05 11:30:14 +0300

    v2 исправлены недочёты

--- js/main.js
+++ js/main.js
@@ -7,35 +7,44 @@ var CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
 var OFFERS_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var MAP_PIN_WIDTH = 50;
-var MAP_PIN_HEIGHT = 70;
+var MAP_PIN_UPPER_Y = 130;
+var MAP_PIN_LOWER_Y = 630;
+var MAP_PIN_LEFTMOST_X = 1;
 
 var offers = [];
 var offersLocation = [];
+var mapWithOffers = document.querySelector('.map');
+var mapPinRightmostX = mapWithOffers.offsetWidth - MAP_PIN_WIDTH - 1;
 
 var getRandomValue = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+var arrayShuffle = function (array) {
+  var counter = array.length;
+  while (counter > 0) {
+    var index = Math.floor(Math.random() * counter);
+    counter--;
+    var temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+};
 var getRundomOptions = function (arrayOptions) {
   var rundomOptions = [];
-  var counter = arrayOptions.length; var temp; var index;
-  while (counter--) {
-    index = (Math.random() * counter) | 0;
-    temp = arrayOptions[counter];
-    arrayOptions[counter] = arrayOptions[index];
-    arrayOptions[index] = temp;
-  }
-  for (var j = 0; j < getRandomValue(0, arrayOptions.length); j++) {
+  arrayShuffle(arrayOptions);
+  for (var j = 0; j <= getRandomValue(0, arrayOptions.length); j++) {
     rundomOptions.push(arrayOptions[j]);
   }
   return rundomOptions;
 };
 
-
 var getOfferLocation = function (numberOfOffersLoacation) {
   for (var k = 0; k < numberOfOffersLoacation; k++) {
-    var x = getRandomValue(130, 1070);
-    var y = getRandomValue(130, 630);
+    var x = getRandomValue(MAP_PIN_LEFTMOST_X, mapPinRightmostX);
+    var y = getRandomValue(MAP_PIN_UPPER_Y, MAP_PIN_LOWER_Y);
     offersLocation.push({x: x,
       y: y});
   }
@@ -44,14 +53,8 @@ var getOfferLocation = function (numberOfOffersLoacation) {
 
 var getNewOffers = function (numberOfOffers) {
   for (var l = 0; l < numberOfOffers; l++) {
-    var offerPrice = getRandomValue(5000, 100000);
-    var offerType = TYPES[getRandomValue(0, 3)];
-    var offerFeatures = getRundomOptions(OFFERS_FEATURES);
-    var offerPhotos = getRundomOptions(PHOTOS);
     var numberOfRooms = getRandomValue(1, 10);
     var numerOfGuest = numberOfRooms * 2;
-    var checkinTime = CHECKIN_TIMES[getRandomValue(0, 2)];
-    var checkOutTime = CHECKOUT_TIMES[getRandomValue(0, 2)];
     offers.push({
       author: {
         avatar: 'img/avatars/user0' + (l + 1) + '.png',
@@ -59,15 +62,15 @@ var getNewOffers = function (numberOfOffers) {
       offer: {
         title: 'заголовок предложения 0' + (l + 1),
         address: offersLocation[l].x + ', ' + offersLocation[l].y,
-        price: offerPrice,
-        type: offerType,
+        price: getRandomValue(5000, 100000),
+        type: TYPES[getRandomValue(0, 3)],
         rooms: numberOfRooms,
         guests: numerOfGuest,
-        checkin: checkinTime,
-        checkout: checkOutTime,
-        features: offerFeatures,
+        checkin: CHECKIN_TIMES[getRandomValue(0, 2)],
+        checkout: CHECKOUT_TIMES[getRandomValue(0, 2)],
+        features: getRundomOptions(OFFERS_FEATURES),
         description: 'описание предложения 0' + (l + 1),
-        photos: offerPhotos
+        photos: getRundomOptions(PHOTOS)
       }
     });
   }
@@ -81,7 +84,7 @@ var addPinsToPage = function (offersQuantity) {
   for (var m = 0; m < offersQuantity; m++) {
     var pinElement = pinTemplate.cloneNode(true);
     pinElement.querySelector('.map__pin').style.left = (offersLocation[m].x + MAP_PIN_WIDTH / 2) + 'px';
-    pinElement.querySelector('.map__pin').style.top = (offersLocation[m].y + MAP_PIN_HEIGHT) + 'px';
+    pinElement.querySelector('.map__pin').style.top = (offersLocation[m].y) + 'px';
     pinElement.querySelector('img').src = offers[m].author.avatar;
     pinElement.querySelector('img').alt = offers[m].offer.title;
     fragment.appendChild(pinElement);
@@ -89,7 +92,7 @@ var addPinsToPage = function (offersQuantity) {
   document.querySelector('.map__pins').appendChild(fragment);
 };
 
-var mapWithOffers = document.querySelector('.map');
+
 mapWithOffers.classList.remove('map--faded');
 getOfferLocation(NUMBER_OF_OFFERS);
 getNewOffers(NUMBER_OF_OFFERS);
