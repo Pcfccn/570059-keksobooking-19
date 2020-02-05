197ea938a03044a856979f0e12ff54ce627852d5
unknown <kraynevroman@gmail.com>
2020-02-05 14:40:14 +0300

    v4

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 'use strict';
 
-var NUMBER_OF_OFFERS = 8;
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
-var CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
-var OFFERS_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var MAP_PIN_WIDTH = 50;
-var MAP_PIN_UPPER_Y = 130;
-var MAP_PIN_LOWER_Y = 630;
-var MAP_PIN_LEFTMOST_X = 1;
-
+var INITIAL_DATA = {
+  NUMBER_OF_OFFERS: 8,
+  TYPES: ['palace', 'flat', 'house', 'bungalo'],
+  CHECKIN_TIMES: ['12:00', '13:00', '14:00'],
+  CHECKOUT_TIMES: ['12:00', '13:00', '14:00'],
+  OFFERS_FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  PHOTOS: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+  MAP_PIN_WIDTH: 50,
+  MAP_PIN_UPPER_Y: 130,
+  MAP_PIN_LOWER_Y: 630,
+  MAP_PIN_LEFTMOST_X: 1
+};
 var offers = [];
 var offersLocation = [];
 var mapWithOffers = document.querySelector('.map');
-var mapPinRightmostX = mapWithOffers.offsetWidth - MAP_PIN_WIDTH - 1;
+var mapPinRightmostX = mapWithOffers.offsetWidth - INITIAL_DATA.MAP_PIN_WIDTH - 1;
 
 var getRandomValue = function (min, max) {
   min = Math.ceil(min);
@@ -45,8 +46,8 @@ var getRundomOptions = function (arrayOptions) {
 
 var getOfferLocation = function (numberOfOffersLoacation) {
   for (var k = 0; k < numberOfOffersLoacation; k++) {
-    var x = getRandomValue(MAP_PIN_LEFTMOST_X, mapPinRightmostX);
-    var y = getRandomValue(MAP_PIN_UPPER_Y, MAP_PIN_LOWER_Y);
+    var x = getRandomValue(INITIAL_DATA.MAP_PIN_LEFTMOST_X, mapPinRightmostX);
+    var y = getRandomValue(INITIAL_DATA.MAP_PIN_UPPER_Y, INITIAL_DATA.MAP_PIN_LOWER_Y);
     offersLocation.push({x: x,
       y: y});
   }
@@ -65,14 +66,14 @@ var getNewOffers = function (numberOfOffers) {
         title: 'заголовок предложения 0' + (l + 1),
         address: offersLocation[l].x + ', ' + offersLocation[l].y,
         price: getRandomValue(5000, 100000),
-        type: TYPES[getRandomValue(0, 3)],
+        type: INITIAL_DATA.TYPES[getRandomValue(0, 3)],
         rooms: numberOfRooms,
         guests: numerOfGuest,
-        checkin: CHECKIN_TIMES[getRandomValue(0, 2)],
-        checkout: CHECKOUT_TIMES[getRandomValue(0, 2)],
-        features: getRundomOptions(OFFERS_FEATURES),
+        checkin: INITIAL_DATA.CHECKIN_TIMES[getRandomValue(0, 2)],
+        checkout: INITIAL_DATA.CHECKOUT_TIMES[getRandomValue(0, 2)],
+        features: getRundomOptions(INITIAL_DATA.OFFERS_FEATURES),
         description: 'описание предложения 0' + (l + 1),
-        photos: getRundomOptions(PHOTOS)
+        photos: getRundomOptions(INITIAL_DATA.PHOTOS)
       }
     });
   }
@@ -96,6 +97,6 @@ var addPinsToPage = function (offersQuantity) {
 
 
 mapWithOffers.classList.remove('map--faded');
-getOfferLocation(NUMBER_OF_OFFERS);
-getNewOffers(NUMBER_OF_OFFERS);
-addPinsToPage(NUMBER_OF_OFFERS);
+getOfferLocation(INITIAL_DATA.NUMBER_OF_OFFERS);
+getNewOffers(INITIAL_DATA.NUMBER_OF_OFFERS);
+addPinsToPage(INITIAL_DATA.NUMBER_OF_OFFERS);
